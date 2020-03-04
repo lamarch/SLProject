cb937da795f1a39fccc675831d1626a3d8abb184
lamarch <42736089+p0lohx@users.noreply.github.com>
2020-03-04 17:18:25 +0100

    update gitignore

---
+++ SLCompilerLib/Patches/BasePatches.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SLProject.SLCompilerLib.Patches
+{
+    public class BasePatches
+    {
+
+        public bool 
+
+    }
+}
